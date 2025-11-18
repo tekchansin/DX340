390976d3d5430b440289ec850ecf552382348658
charnsilp_c <charnsilp_c@Tek-Macbook-Air-M4.local>
2025-11-18 13:56:38 +0700

    Add Debloat script

---
+++ debloat.sh
@@ -0,0 +1,90 @@
+#!/system/bin/sh
+
+# ===============================================
+# tekChansin's DX340 Audiophile Debloat Script
+# Listed privided by mattclarke18 on Head-fi
+#
+# ===============================================
+
+# Check for Root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Error: This script must be run as root."
+  echo "Please run 'su' first, then run this script."
+  exit 1
+fi
+
+echo "--- Starting DX340 Audiophile Debloat ---"
+echo "tekChansin's DX340 Audiophile Debloat Script"
+echo "Debloat List from mattclarke18 on Head-fi"
+echo "Disabling 'Noise Maker' apps and bloatware..."
+
+# Debloat List
+PACKAGES_TO_DISABLE="
+android.overlay.common
+com.android.backupconfirm
+com.android.bips
+com.android.bluetoothmidiservice
+com.android.bookmarkprovider
+com.android.calculator2
+com.android.companiondevicemanager
+com.android.deskclock
+com.android.dreams.basic
+com.android.htmlviewer
+com.android.inputdevices
+com.android.internal.display.cutout.emulation.corner
+com.android.internal.display.cutout.emulation.double
+com.android.internal.display.cutout.emulation.hole
+com.android.internal.display.cutout.emulation.tall
+com.android.internal.display.cutout.emulation.waterfall
+com.android.internal.systemui.navbar.gestural
+com.android.internal.systemui.navbar.gestural_extra_wide_back
+com.android.internal.systemui.navbar.gestural_narrow_back
+com.android.internal.systemui.navbar.gestural_wide_back
+com.android.managedprovisioning
+com.android.musicfx
+com.android.providers.blockednumber
+com.android.providers.partnerbookmarks
+com.android.providers.userdictionary
+com.android.server.telecom.overlay.common
+com.android.sharedstoragebackup
+com.android.theme.font.notoserifsource
+com.android.traceur
+com.android.vpndialogs
+com.android.wallpaper.livepicker
+com.android.wallpaperbackup
+com.android.wallpapercropper
+com.google.android.apps.restore
+com.google.android.feedback
+com.google.android.gms.location.history
+com.google.android.onetimeinitializer
+com.google.android.printservice.recommendation
+com.google.android.syncadapters.calendar
+com.ibasso.caputurelog
+com.ibasso.updater
+com.qti.qualcomm.mstatssystemservice
+com.qualcomm.qti.devicestatisticsservice
+com.qualcomm.qti.dynamicddsservice
+com.qualcomm.qti.poweroffalarm
+com.qualcomm.qti.qms.service.connectionsecurity
+com.qualcomm.qti.qms.service.trustzoneaccess
+com.qualcomm.timeservice
+com.qualcomm.wfd.service
+org.lineageos.setupwizard
+"
+
+# 3. "วนลูป" (Loop) และ "ปิด" (Disable) ทีละตัว
+for pkg in $PACKAGES_TO_DISABLE; do
+  echo -n "Disabling $pkg... "
+
+  pm disable "$pkg"
+  
+  # Checking
+  if [ $? -eq 0 ]; then
+    echo "OK."
+  else
+    echo "Failed (Maybe not found or already disabled)."
+  fi
+done
+
+echo "--- Debloat Complete ---"
+echo "Please Reboot your device NOW"
